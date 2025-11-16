eccafd818e42f732033ccaf6fee90938452fe3a2
Maynard Peralta <maynperalta@gmail.com>
2025-11-16 00:01:13 -0700

    Added test methods to Main.java

--- Module1/src/Main/Main.java
+++ Module1/src/Main/Main.java
@@ -2,8 +2,16 @@ package Main;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		CheckingAccount userAccount = new CheckingAccount(1.25);
+		
+		userAccount.setFirstName("John");
+		userAccount.setLastName("Smith");
+		userAccount.setAccountID(8675309);
+		
+		userAccount.deposit(100.00);
+		userAccount.withdrawal(50.00);
+		userAccount.processWithdrawal(75.00);
+		
+		userAccount.displayAccount();
 	}
-
 }
