22e3e9aaf790fcb9d15a97de7f2ca7521ce45f68
Maynard Peralta <maynperalta@gmail.com>
2025-11-16 14:22:55 -0700

    Third test case with different data.

--- Module1/src/Main/Main.java
+++ Module1/src/Main/Main.java
@@ -4,15 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		CheckingAccount userAccount = new CheckingAccount(1.25); // new class instance
 	// new account details test methods
-		userAccount.setFirstName("Chuck");
-		userAccount.setLastName("Jones");
-		userAccount.setAccountID(12345);
+		userAccount.setFirstName("Rosetta");
+		userAccount.setLastName("Tharpe");
+		userAccount.setAccountID(45678910);
 	// show account information	
 		userAccount.displayAccount();
 	// deposit and withdrawal test methods	
-		userAccount.deposit(0.00);
-		userAccount.withdrawal(50.00);
+		userAccount.deposit(200.00);
+		userAccount.withdrawal(85.00);
 	// overdraft fee assessment from CheckingAccount file	
-		userAccount.processWithdrawal(75.00);
+		userAccount.processWithdrawal(30.00);
 	}
 }
